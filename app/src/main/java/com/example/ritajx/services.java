b093c29843245cc09e3d8b0006f345b2fcaa2af2
Ghaid Yahya <132503835+GhaidYahya@users.noreply.github.com>
2024-01-12 19:19:56 +0200

    GhaidServicesUpdate

--- app/src/main/java/com/example/ritajx/services.java
+++ app/src/main/java/com/example/ritajx/services.java
@@ -10,12 +10,29 @@ import android.widget.Button;
 public class services extends AppCompatActivity {
 
     private Button gym_btn;
+    private Button bank_btn;
+    private Button library_btn;
+
+    private Button clinic_btn;
+
+    private Button mytodo_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_services);
         gym_btn= findViewById(R.id.gym_btn);
+        gym_btn = findViewById(R.id.gym_btn);
+        bank_btn = findViewById(R.id.bank_btn);
+        library_btn = findViewById(R.id.library_btn);
+        clinic_btn = findViewById(R.id.clinic_btn);
+        mytodo_btn = findViewById(R.id.mytodo_btn);
 
+        // Set background colors
+        gym_btn.setBackgroundTintList(getResources().getColorStateList(R.color.purple_200));
+        bank_btn.setBackgroundTintList(getResources().getColorStateList(R.color.sage_green));
+        library_btn.setBackgroundTintList(getResources().getColorStateList(R.color.purple_500));
+        clinic_btn.setBackgroundTintList(getResources().getColorStateList(R.color.pink));
+        mytodo_btn.setBackgroundTintList(getResources().getColorStateList(R.color.lav));
         gym_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
