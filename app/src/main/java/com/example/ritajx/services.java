7a7fe5f2e0489c6fed4ecbd63539b2b84460ff8f
Ghaid Yahya <132503835+GhaidYahya@users.noreply.github.com>
2024-02-10 19:48:12 +0200

    GhaidOtherNav

--- app/src/main/java/com/example/ritajx/services.java
+++ app/src/main/java/com/example/ritajx/services.java
@@ -1,17 +1,29 @@
 package com.example.ritajx;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class services extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class services extends AppCompatActivity  implements NavigationView.OnNavigationItemSelectedListener{
 
     private Button gym_btn;
     private Button bank_btn;
@@ -20,18 +32,19 @@ public class services extends AppCompatActivity {
     private Button clinic_btn;
 
     private Button mytodo_btn;
+    ImageButton profileButton2;
 
 
     private ImageButton navButton;
-    private LinearLayout drawerLayout;
-    private Animation slideInAnimation;
-    private Animation slideOutAnimation;
-    private boolean isDrawerOpen = false;
+    DrawerLayout drawerLayout;
+    String userID;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_services);
+        setContentView(R.layout.drawer_nav2);
         gym_btn = findViewById(R.id.gym_btn);
         gym_btn = findViewById(R.id.gym_btn);
         bank_btn = findViewById(R.id.bank_btn);
@@ -40,10 +53,10 @@ public class services extends AppCompatActivity {
         mytodo_btn = findViewById(R.id.mytodo_btn);
 
         navButton = findViewById(R.id.navv_btn);
-        drawerLayout = findViewById(R.id.nav_drawer);
+        drawerLayout = findViewById(R.id.drawer_nav);
 
-        slideInAnimation = AnimationUtils.loadAnimation(this, R.anim.slide_in);
-        slideOutAnimation = AnimationUtils.loadAnimation(this, R.anim.slide_out);
+
+
 
         // Set background colors
         gym_btn.setBackgroundTintList(getResources().getColorStateList(R.color.purple_200));
@@ -53,6 +66,24 @@ public class services extends AppCompatActivity {
 
 
         mytodo_btn.setBackgroundTintList(getResources().getColorStateList(R.color.lav));
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        MenuItem homeNavItem = navigationView.getMenu().findItem(R.id.nav_home);
+        MenuItem serviNavItem = navigationView.getMenu().findItem(R.id.nav_servi);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        userID = getIntent().getStringExtra("userID");
+        if (userID==null){
+            userID=getUserIDFromSharedPreferences();
+        }
+        navButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                nav();
+            }
+        });
+
+
 
         gym_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -85,6 +116,15 @@ public class services extends AppCompatActivity {
             }
         });
 
+        profileButton2 = findViewById(R.id.profile_Small_btn);
+        profileButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(services.this, profile.class);
+                intent.putExtra("userID", userID); // Assuming userID is a string containing the user's ID
+                startActivity(intent);
+            }
+        });
 
         mytodo_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -94,19 +134,100 @@ public class services extends AppCompatActivity {
             }
         });
 
-        navButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (isDrawerOpen) {
-                    drawerLayout.startAnimation(slideOutAnimation);
-                    drawerLayout.setVisibility(View.GONE);
-                } else {
-                    drawerLayout.setVisibility(View.VISIBLE);
-                    drawerLayout.startAnimation(slideInAnimation);
-                }
-                isDrawerOpen = !isDrawerOpen;
-            }
-        });
+
 
     }
+
+    private void nav() {
+        if (drawerLayout != null) {
+            Log.d("DrawerStatus", "Drawer is not null, opening...");
+            drawerLayout.openDrawer(GravityCompat.START);
+        } else {
+            Log.e("DrawerStatus", "Drawer is null!");
+        }
+    }
+    private void openHomeActivity() {
+        Intent intent = new Intent(this, home.class);
+        startActivity(intent);
+    }
+
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        DrawerLayout drawer = findViewById(R.id.drawer_nav);
+
+        switch (item.getTitle().toString()) {
+            case "Home":
+                openHomeActivity();
+                break;
+
+            case "Services":
+                drawer.closeDrawer(GravityCompat.START);
+                break;
+
+            case "Settings":
+                Intent profileIntent = new Intent(services.this, profile.class);
+                startActivity(profileIntent);
+
+                break;
+
+            case "Share with friends":
+                String appLink = "http://RitajX_Downaload_App";
+                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("http://RitajX_Downaload_App", appLink);
+                clipboard.setPrimaryClip(clip);
+                Toast.makeText(getApplicationContext(), "RitajX link copied to clipboard :)", Toast.LENGTH_SHORT).show();
+
+
+                break;
+            case "About Us":
+
+
+                break;
+
+            case "My Schedule":
+
+
+                break;
+            case "My Grades":
+                Intent gradesIntent = new Intent(services.this, Grade.class);
+                startActivity(gradesIntent);
+
+
+                break;
+            case "Weather":
+                Intent weatherIntent = new Intent(services.this, weatherActivity.class);
+                startActivity(weatherIntent);
+
+                break;
+            case "Currency Exchange":
+                Intent bankIntent = new Intent(services.this, Bank2.class);
+                startActivity(bankIntent);
+
+                break;
+            case "BMI":
+                Intent mbiIntent = new Intent(services.this, BMI.class);
+                startActivity(mbiIntent);
+
+
+                break;
+
+            case "Logout":
+                Intent logOutIntent = new Intent(services.this, MainActivity.class);
+                startActivity(logOutIntent);
+                finish();
+
+                break;
+
+            default:
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    private String getUserIDFromSharedPreferences() {
+        SharedPreferences sharedPreferences = getSharedPreferences("userid", MODE_PRIVATE);
+        // Return null as default value if "userID" not found
+        return sharedPreferences.getString("userID", null);
+    }
+
 }
